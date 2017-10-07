d922bc6d13caa42e1aab5278356dfa392add0e21
saiprasanthk <saiprasanth@smartsoft.in>
2017-10-07 11:46:19 +0530

    initial commit

--- app/src/main/java/com/mobileapp/testproject/MainActivity.java
+++ app/src/main/java/com/mobileapp/testproject/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    EditText editText;
+    EditText editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
